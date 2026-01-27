ba088003efd1b046ebbea97d517da9c3c77b2002
Sphesihle Mabaso <mabasosphesihle25@gmail.com>
2026-01-27 18:01:52 +0200

    Added more projects

--- portfolio/src/Projects/Projects_array.jsx
+++ portfolio/src/Projects/Projects_array.jsx
@@ -1,14 +1,28 @@
 const Projects_array = [
   {
     title: "Portfolio Website",
-    description: "A professional developer portfolio built with React and modular CSS, showcasing clean UI components , responsive design and a dynamic contact form powered by EmailJS for real-time client communication.",
-    image: "/images/Screenshot.png"
+    description:
+      "A professional developer portfolio built with React and modular CSS, showcasing clean UI components , responsive design and a dynamic contact form powered by EmailJS for real-time client communication.",
+    image: "/images/Screenshot.png",
   },
   {
     title: "E-commerce Web App",
-    description: "A full-stack shopping platform with user authentication using Oauth2.0 and bcrypt, Admin Dashboard,Payment System(Payfast).",
+    description:
+      "A full-stack shopping platform with user authentication using Oauth2.0 and bcrypt, Admin Dashboard,Payment System(Payfast).",
     image: "/images/SphesihleCars.png",
-    link: "https://sphesihlecars.onrender.com"
+    link: "https://sphesihlecars.onrender.com",
+  },
+  {
+    title: "ThinkPad Notes",
+    description:
+      "A note-taking app built with React, Node.js, TypeScript, and PostgreSQL, enabling users to create, edit, and organize notes with a clean, responsive interface.",
+    image: "/images/Thinkboard App.png",
+  },
+  {
+    title: "Tri Scope Planner",
+    description:
+      "A scheduling web app built with React (Vite), Node.js, TypeScript, PostgreSQL, Tailwind CSS, and Shadcn/UI, allowing daily, weekly, and monthly task planning with Google Calendar integration.",
+    image: "/images/Tri Scope Planner.png",
   },
 ];
-export default Projects_array
+export default Projects_array;
