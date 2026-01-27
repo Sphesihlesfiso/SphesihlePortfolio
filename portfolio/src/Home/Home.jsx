05c95a3592e7dc71c41b5e73d31ac2c2f9031f39
Sphesihle Mabaso <mabasosphesihle25@gmail.com>
2026-01-27 18:31:01 +0200

    updated cv

--- portfolio/src/Home/Home.jsx
+++ portfolio/src/Home/Home.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
-import Typewriter from 'typewriter-effect';
-import './Home.css';
+import React from "react";
+import Typewriter from "typewriter-effect";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div className='Home-container'>
-      <div className='Picture-paragraph'>
-        <div className='Picture'>
-          <img className="Home-img" src={`${process.env.PUBLIC_URL}/images/MyPhoto.jpg`} alt="Sphesihle Mabaso" />
-
+    <div className="Home-container">
+      <div className="Picture-paragraph">
+        <div className="Picture">
+          <img
+            className="Home-img"
+            src={`${process.env.PUBLIC_URL}/images/MyPhoto.jpg`}
+            alt="Sphesihle Mabaso"
+          />
         </div>
 
-        <div className='Text'>
+        <div className="Text">
           <h3>
-            Hi, I'm <span className='Highlighted-text'>Sphesihle Mabaso</span> a
+            Hi, I'm <span className="Highlighted-text">Sphesihle Mabaso</span> a
           </h3>
 
-          <div className='Home-paragraph'>
+          <div className="Home-paragraph">
             <Typewriter
               options={{
                 strings: [
-                  'Software Engineer in training',
-                  'Problem Solver',
-                  'Team Player',
-                  'Full-Stack Developer'
+                  "Software Engineer in training",
+                  "Problem Solver",
+                  "Team Player",
+                  "Full-Stack Developer",
                 ],
                 autoStart: true,
                 loop: true,
@@ -31,17 +34,16 @@ export default function Home() {
             />
           </div>
 
-          <p className='Home-paragraph'>
-            I'm passionate about building secure, web applications and websites that solve real-world problems.Whether you want a buisness or personal website you've found  the right person for the job.
+          <p className="Home-paragraph">
+            I'm passionate about building secure, web applications and websites
+            that solve real-world problems.Whether you want a buisness or
+            personal website you've found the right person for the job.
           </p>
-
-         
         </div>
       </div>
 
-      <div className='Buttons'>
-            <a href={`${process.env.PUBLIC_URL}/Sphesihle_Mabaso.docx`} download>
-
+      <div className="Buttons">
+        <a href={`${process.env.PUBLIC_URL}/Sphesihle_Mabaso.docx`} download>
           <button>Get Resume</button>
         </a>
         <a href="#contact">
@@ -51,4 +53,3 @@ export default function Home() {
     </div>
   );
 }
-
